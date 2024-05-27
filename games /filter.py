b02529689a1164acb053655c9bd0eafb7ed228ba
louis mulaa <louismulaa06@gmail.com>
2024-05-27 14:15:25 +0300

    map function

---
+++ games /filter.py
@@ -0,0 +1,18 @@
+#filter ...inshort inachunga tu 
+
+friends = [("Rachel", 19),
+           ("Monica", 18),
+           ("Phoebe", 17),
+           ("Joey", 16),
+           ("Chandler", 21),
+           ("Rose", 20)]
+
+# Define a lambda function to filter friends aged 18 or above
+is_old_enough = lambda data: data[1] >= 18
+
+# Use filter() to apply the lambda function to the list of friends
+drinking_friends = list(filter(is_old_enough, friends))
+
+# Print the list of friends who are old enough to drink
+for friend in drinking_friends:
+    print(friend)
